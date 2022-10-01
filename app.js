f683d9db832ab0a213022b6fbb8017aa46345391
Sushmita <sushmitakumari363@gmail.com>
2022-10-01 12:57:49 +0530

    refactor

--- app.js
+++ app.js
@@ -1,47 +1,75 @@
 const billAmount = document.querySelector("#bill-amount");
-const givenAmount = document.querySelector("#cash-given");
-const checkbtn = document.querySelector("#check-button")
-const displayResult = document.querySelector("#result")
-const numberOfNotes = document.querySelectorAll(".no-of-notes");
-const availableNotes = [2000 , 500 , 100 , 50 , 20 , 10 , 5 , 1 ]
-//console.log(givenAmount.value)
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message1 = document.querySelector("#message-one");
+
+const message2 = document.querySelector("#message-two");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const proceedBtn = document.querySelector("#proceed");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const toggleElement = document.querySelector("#toggle-element");
+const changeTable = document.querySelector(".change-table");
+toggleElement.style.display = "none";
+
+function tableOpen() {
+
+  if (billAmount.value) {
+    hideMessage1();
+    toggleElement.style.display = "block";
+    checkButton.addEventListener("click", function validateBillAndCashAmount() {
+      hideMessage2();
+
+      changeTable.style.display = "none";
+      if (Number(cashGiven.value) >= Number(billAmount.value) && Number(billAmount.value) > 0) {
 
 
-function checkValidity(){
-    hideMessage();
-    if ( billAmount.value > 0 )
-    {
-        if (   Number(givenAmount.value)  > Number(billAmount.value)){
-            
-            const calculateChange = givenAmount.value - billAmount.value;
-            result("you have change left with "+calculateChange +" Rs...")
-            displayAmount(calculateChange);
-        }else if(givenAmount.value === billAmount.value){
-            result("You have no exchange left")
-            
-        }
-        else{
-            result("Do you wanna wash plates? Or\n  GIVE SOME CASH")
-        }
-    }else{
-        result("give correct amount")
-    }
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        caluclateChange(amountToBeReturned);
+        changeTable.style.display = "block";
+
+      } else {
+        showMessage2("Do you wanna wash plates?");
+        changeTable.style.display = "none";
+
+      }
+    });
+  } else {
+    showMessage1("please enter valid values");
+  }
+}
+proceedBtn.addEventListener("click", tableOpen);
+changeTable.style.display = "none";
+
+
+
+function caluclateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    noOfNotes[i].innerHTML = "";
+    const numberOfNotes = Math.trunc(
+      amountToBeReturned / availableNotes[i]);
+
+    amountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+
 }
 
-function result(msg)
-{
-    displayResult.style.display="block";
-    displayResult.innerText = msg;
+function showMessage1(msg) {
+  message1.style.display = "block";
+  message1.innerText = msg;
 }
-function hideMessage(){
-    displayResult.style.display = "none";
+
+function hideMessage2() {
+  message2.style.display = "none";
+
 }
-function displayAmount(calculateChange){
-    for(let i = 0 ; i < availableNotes.length; i++)
-    {
-        const noOfNotes = Math.trunc(calculateChange / availableNotes[i]);
-        calculateChange %= availableNotes[i];
-        numberOfNotes[i].innerText = noOfNotes;
-    }
+
+function hideMessage1() {
+  message1.style.display = "none";
+
 }
-checkbtn.addEventListener("click",checkValidity);
+
+function showMessage2(msg) {
+  message2.style.display = "block";
+  message2.innerText = msg;
+}
